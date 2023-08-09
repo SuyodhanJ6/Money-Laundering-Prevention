0e73c795fd89f85e837dbcf46848c0624eb74e19
SuyodhanJ6 <prashantmalge181@gmail.com>
2023-08-09 11:08:10 +0530

    Adding  Flowchart

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from script.exception import MoneyLaunderingException
 from script.pipeline import training_pipeline
 from script.pipeline.training_pipeline import TrainPipeline
 
+from fastapi import FastAPI
+
 if __name__ == '__main__':
     try:
         training_pieline = TrainPipeline()
